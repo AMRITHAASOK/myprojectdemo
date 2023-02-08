8ca311f41fd198325eff6bc314817287d67b3003
amrithaasok <amrithaasok.aa@gamil.com>
2023-02-08 18:24:32 +0530

    myproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',//html template
+  styleUrls: ['./app.component.css']//provides style sheets
 })
 export class AppComponent {
   title = 'myproject';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
+//
+
+//decorators - Its a function to hold component data...attach meta data..
+//  meta data- class declarations,methods,properties start with @
 @NgModule({
   declarations: [
-    AppComponent
+    //To hold components,directives of particular modules..
+    AppComponent,
+    TestComponent,
+   
   ],
+//it contains 3rd party libraries
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//it provides services
+  bootstrap: [AppComponent] //To identify the root component
 })
 export class AppModule { }
+//Specifies the declarations and modules which 
+//is available for a component in another module
